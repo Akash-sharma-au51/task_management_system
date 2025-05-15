14da90f1ba2020ddcf43158866eefd18e86339d4
Akash Sharma <sharmaakash11401@gmail.com>
2025-05-15 17:30:14 +0530

    changs made

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require("dotenv")
 const userRoutes = require("./routes/userRoutes")
 const taskRoute = require("./routes/tasksRoutes")
@@ -11,6 +12,7 @@ const port = process.env.PORT ||8000
 const app = express()
 
 //middlewares
+app.use(cors())
 app.use(express.json())
 app.use("/api/users",userRoutes)
 app.use("/api/tasks",taskRoute)
@@ -26,4 +28,15 @@ connecttoDb().then(()=>{
     console.error("connection failed");
     
 })
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+});
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+});
 
